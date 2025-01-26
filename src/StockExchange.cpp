69beea5df8c590c70a47183979d5d9cc14b50105
Sasha <beznal@stall.com.ua>
2025-01-26 23:47:05 +0200

    add ssl and sql lib

--- src/StockExchange.cpp
+++ src/StockExchange.cpp
@@ -1,14 +1,21 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <iostream>
 #include <httplib.h>
+#include <openssl/opensslv.h>
 
 int main(int argc, char* argv[])
 {
+	//httplib::SSLServer svr("D:/StockExchange/src/cert.pem", "D:/StockExchange/src/key.pem");
 	httplib::SSLServer svr("./cert.pem", "./key.pem");
 	//httplib::Server svr;
 
+	if (!svr.is_valid()) {
+		std::cerr << "Invalid SSL server configuration!" << std::endl;
+		return 1;
+	}
+
 	svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
-		res.set_content("Hello World!", "text/plain");
+		res.set_content("Hello World1!2", "text/plain");
 		});
 
 	!svr.listen("0.0.0.0", 8080) ? (std::cerr << "Server failed to start!" << std::endl) : (std::cout << "Server is running!" << std::endl);
