f9afd97b71c07fcc24014a32e99b2561b1892eca
Mykhailo Didur <mikhaildidur2003@gmail.com>
2025-02-02 09:20:31 +0200

    Fix sources

--- src/StockExchange.cpp
+++ src/StockExchange.cpp
@@ -1,5 +1,6 @@
 // #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <iostream>
+#include <thread>
 #include <httplib.h>
 // #include <openssl/opensslv.h>
 
@@ -15,9 +16,18 @@ int main(int argc, char* argv[])
 
 	svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
 		res.set_content("Hello World1!2", "text/plain");
-		});
+	});
 
-	!svr.listen("0.0.0.0", 8080) ? (std::cerr << "Server failed to start!" << std::endl) : (std::cout << "Server is running!" << std::endl);
-	
+	std::thread th([&svr](){
+		auto res = svr.listen("0.0.0.0", 8080);
+	});
+
+	while(true) {
+		std::cout << "I am alive" << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(10));
+	}
+
+	th.join();
+
 	return 0;
 }
